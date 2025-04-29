e026bf10b26738541d50777b6cd8ee6a1972ce20
HenriqueBent0 <henrique.bt33@gmail.com>
2025-04-29 10:02:23 -0300

    feat: 100% de cobertura na classe Calculadora

--- src/test/java/model/CalculadoraTest.java
+++ src/test/java/model/CalculadoraTest.java
@@ -28,5 +28,27 @@ public class CalculadoraTest {
         double retornoFeito = calculadora.getDiferenca();
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
-    
+    @Test
+    void testGetProduto(){
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    @Test
+    void TestGetQuociente(){
+       Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getQuociente();
+        assertEquals(retornoEsperado, retornoFeito, 0); 
+    }
+    @Test
+    void testConstrutorPadraoInicializaComZero() {
+        Calculadora calc = new Calculadora();
+
+        assertEquals(0.0, calc.getValorA(), "Valor A deve ser 0.0");
+        assertEquals(0.0, calc.getValorB(), "Valor B deve ser 0.0");
+    }
 }
+    
+
